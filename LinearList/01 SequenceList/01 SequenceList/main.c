055539635b1dab843fc88b202e664f365234fd4c
LeungyWang <yuan_leung@icloud.com>
2021-10-17 20:35:08 +0800

    fix bug

--- LinearList/01 SequenceList/01 SequenceList/main.c
+++ LinearList/01 SequenceList/01 SequenceList/main.c
@@ -5,11 +5,11 @@
 //  Created by leung on 2021/10/17.
 //
 
-
 #include <stdio.h>
-#include "SequenceList.c"
+#include "SequenceList.h"
 
 int main(int argc, const char * argv[]) {
     SqList L;
+    printf("Hello World");
     return 0;
 }
