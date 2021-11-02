7ccfd05f50349dd3c152e56e74cc6ad8d254ca69
LeungyWang <yuan_leung@icloud.com>
2021-11-02 13:15:40 +0800

    Update LinkList

--- LinearList/02 LinkList/02 LinkList/LinkList.c
+++ LinearList/02 LinkList/02 LinkList/LinkList.c
@@ -126,4 +126,102 @@ Input:
 Return:(int)    函数执行状态码,
           1为成功, 0为失败
 **************************************************/
+Status ListInsert(LinkList *L, int i, ElemType e)
+{
+    int j;
+    LinkList p,s;
+    p = *L;
+    j = 1;
+    while (p && j < i) {    /*遍历寻找第i-1个结点*/
+        p = p->next;
+        ++j;
+    }
+    if (!p || j > i) {
+        return  ERROR;  /*第i个结点不存在*/
+    }
+    s = (LinkList)malloc(sizeof(Node)); /*生成新结点(C标准函数)*/
+    s->data = e;
+    s->next = p->next;  /*将p的后继结点赋值给s的后继*/
+    p->next = s;    /*将s赋值给p的后继*/
+    return  OK;
+}
+
+/*************************************************
+Function: LinkListDelete
+Description: 删除链表L中的第i个结点, 并用e返回其值，
+         L的长度减1.
+Input:
+    (LinklList)    *L
+    (Int)           i
+    (ElemType)      *e
+Return:(int)    函数执行状态码,
+          1为成功, 0为失败
+**************************************************/
+Status LinkListDelete(LinkList *L, int i, ElemType *e)
+{
+    int j;
+    LinkList p, q;
+    p = *L;
+    j = 1;
+    while (p->next && j < i) {  /*遍历寻找第i-1个结点*/
+        p = p->next;
+        j++;
+    }
+    if (!(p->next) || j > i) {
+        return ERROR;           /*第i个结点不存在*/
+    }
+    q = p->next;
+    p->next = q->next;  /*将q的后继赋值给p的后继*/
+    *e = q->data;       /*将q结点中的数据给e*/
+    free(q);            /*让系统回收此结点, 释放内存*/
+    return OK;
+}
+
+/*************************************************
+Function: CreateListHead
+Description: 随机产生n个元素的值, 建立
+         带表头结点的单链线性表L(头插法)
+Input:
+    (LinklList)    *L
+    (Int)           n
+**************************************************/
+void CreateListHead(LinkList *L, int n)
+{
+    LinkList p;
+    int i;
+    srand(time(0));     /*初始化随机数种子*/
+    *L = (LinkList)malloc(sizeof(Node));
+    (*L)->next = NULL;  /*先建立一个带头结点的单链表*/
+    for (i=0; i<n; i++) {
+        p = (LinkList)malloc(sizeof(Node)); /*随机生成100以内的数字*/
+        p->data = rand()%100 + 1;
+        p->next = (*L)->next;
+        (*L)->next = p;     /*插入到表头*/
+    }
+}
+
+/*************************************************
+Function: CreateListTail
+Description: 随机产生n个元素的值, 建立
+         带表头结点的单链线性表L(尾插法)
+Input:
+    (LinklList)    *L
+    (Int)           n
+**************************************************/
+void CreateListTail(LinkList *L, int n)
+{
+    LinkList p, r;
+    int i;
+    srand(time(0));     /*初始化随机数种子*/
+    *L = (LinkList)malloc(sizeof(Node));     /*为整个线性表*/
+    r = *L;     /*r为指向尾部的结点*/
+    for (i=0; i<n; i++) {
+        p = (Node *)malloc(sizeof(Node));   /*生成新结点*/
+        p->data = rand()%100 + 1;       /*随机生成100以内的数字*/
+        r->next = p;        /*将表尾终端结点的指针指向新结点*/
+        r = p;      /*将当前的新结点定义为表尾终端结点*/
+    }
+    r->next = NULL;     /*表示当前链表结束*/
+}
+
 
--- LinearList/02 LinkList/02 LinkList/LinkList.h
+++ LinearList/02 LinkList/02 LinkList/LinkList.h
@@ -10,6 +10,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "../../Status/Status/Status.h"
 
 typedef int ElemType;       /*ElemType 类型根据实际情况而定, 这里根据实际情况而定，这里假设为int,**/
