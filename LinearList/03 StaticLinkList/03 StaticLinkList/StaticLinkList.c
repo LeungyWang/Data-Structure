f233a3dde49ccf2f7064885e2c1db52f491a64a9
LeungyWang <yuan_leung@icloud.com>
2021-11-10 13:35:26 +0800

    Add StaticLinkList

--- LinearList/03 StaticLinkList/03 StaticLinkList/StaticLinkList.c
+++ LinearList/03 StaticLinkList/03 StaticLinkList/StaticLinkList.c
@@ -6,3 +6,20 @@
 //
 
 #include "StaticLinkList.h"
+
+/*************************************************
+Function:  InitStaticLinkList
+Description:  初始化静态链表
+Input:
+    (StaticLinkList)    *L
+Return: (int)   函数执行状态码,
+          1为成功, 0为失败
+*************************************************/
+Status InitStaticLinkList(StaticLinkList space)
+{
+    int i;
+    for (i=0; i<MAXSIZE-1; i++) {
+        space[i].cur = i + 1;
+    }
+    space[MAXSIZE-1].cur = 0;   /*目前静态链表为空, 最后一个元素的cur为0*/
+}
--- LinearList/03 StaticLinkList/03 StaticLinkList/StaticLinkList.h
+++ LinearList/03 StaticLinkList/03 StaticLinkList/StaticLinkList.h
@@ -9,5 +9,15 @@
 #define StaticLinkList_h
 
 #include <stdio.h>
+#include "../../Status/Status/Status.h"
+
+#define MAXSIZE 1000    /*假设链表的最大长度是1000*/
+typedef int ElemType;
+
+typedef struct
+{
+    ElemType data;
+    int cur;    /*游标(Cursor), 为0时表示无指向*/
+}Component, StaticLinkList[MAXSIZE];
 
 #endif /* StaticLinkList_h */
